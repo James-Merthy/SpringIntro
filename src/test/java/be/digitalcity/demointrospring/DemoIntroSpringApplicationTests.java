5e9d7c6e95002b6170e4fc2b444fb8ce394367ac
Admin <anasala.merthy@gmail.com>
2022-07-26 14:24:36 +0200

    init

---
+++ src/test/java/be/digitalcity/demointrospring/DemoIntroSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package be.digitalcity.demointrospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoIntroSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
